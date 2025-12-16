684f9849955eb41be31970ef7c607c3eb5b5c720
Harisss <hffitriah@gmail.com>
2025-12-17 01:40:32 +0800

    Yeah shouldn't have done that

---
+++ app/Models/penilaian_perusahaan.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Penilaian_perusahaan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'penilaian_perusahaan';           // nama tabel di database
+    protected $primaryKey = 'id_penilaian_perusahaan';   // primary key
+    public $incrementing = true;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'id_penilaian_perusahaan',
+        'id_perusahaan',
+        'id_kriteria',
+        'nilai',
+    ];
+
+    public function perusahaan()
+    {
+        return $this->belongsTo(Perusahaan::class, 'id_perusahaan', 'id_perusahaan');
+    }
+}
